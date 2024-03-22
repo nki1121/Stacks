593e49b99da809592024c028c5dd7d378421fc0c
nki1121 <nkiuniv@gmail.com>
2024-03-22 22:41:50 +0900

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by 中井晋平 on 2024/03/22.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

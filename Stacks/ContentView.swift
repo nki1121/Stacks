665f0e62608ddef67da1b70bebee94a655bf2371
nki1121 <nkiuniv@gmail.com>
2024-03-24 14:11:18 +0900

    クリーパー

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -9,25 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Rectangle()
-                .foregroundColor(.orange)
-                .frame(width: 200, height: 50)
-            Rectangle()
-                .foregroundColor(.red)
-                .frame(width: 180, height: 20)
-            Rectangle()
-                .foregroundColor(.yellow)
-                .frame(width: 180, height: 20)
-            Rectangle()
-                .foregroundColor(.brown)
-                .frame(width: 180, height: 20)
+        ZStack {
             Rectangle()
                 .foregroundColor(.green)
-                .frame(width: 180, height: 20)
-            Rectangle()
-                .foregroundColor(.orange)
-                .frame(width: 200, height: 50)
+                .frame(width: 300, height: 300)
+            VStack {
+                HStack {
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 70, height: 70)
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 70, height: 70)
+                }
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 50, height: 20)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 100, height: 80)
+            }
+            
         }
         .padding()
     }

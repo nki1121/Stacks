d2a5c3c58d0dcc9289c50928080f8b775cc99022
nki1121 <nkiuniv@gmail.com>
2024-03-24 14:31:51 +0900

    ミニオン

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -9,27 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.green)
-                .frame(width: 300, height: 300)
-            VStack {
-                HStack {
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .frame(width: 70, height: 70)
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .frame(width: 70, height: 70)
+        VStack {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 300, height: 300)
+                ZStack {
+                    VStack{
+                        HStack {
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 60, height: 20)
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.gray)
+                                    .frame(width: 90, height: 90)
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 70, height: 70)
+                                Rectangle()
+                                    .foregroundColor(.black)
+                                    .frame(width: 20, height: 20)
+                            }
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.gray)
+                                    .frame(width: 90, height: 90)
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 70, height: 70)
+                                Rectangle()
+                                    .foregroundColor(.black)
+                                    .frame(width: 20, height: 20)
+                            }
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 60, height: 20)
+                        }
+                        ZStack{
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 70, height: 40)
+                            VStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 60, height: 10)
+                                Rectangle()
+                                    .foregroundColor(.red)
+                                    .frame(width: 60, height: 10)
+                            }
+                        }
+                    }
                 }
-                Rectangle()
-                    .foregroundColor(.black)
-                    .frame(width: 50, height: 20)
-                Rectangle()
-                    .foregroundColor(.black)
-                    .frame(width: 100, height: 80)
             }
-            
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 100)
+            HStack {
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+            }
         }
         .padding()
     }

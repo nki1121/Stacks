341d7df30dcb7441c524d7a33432e58f70c9ac4c
nki1121 <nkiuniv@gmail.com>
2024-03-24 14:07:09 +0900

    ハンバーガー

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -11,45 +11,23 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Rectangle()
-                .foregroundColor(.gray)
-                .frame(width: 350, height: 100)
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
             Rectangle()
-                .foregroundColor(.purple)
-                .frame(width: 350, height: 100)
-            HStack {
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .frame(width: 150, height: 150)
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 100, height: 100)
-                Rectangle()
-                    .foregroundColor(.yellow)
-                    .frame(width: 50, height: 50)
-            }
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.green)
-                    .frame(width: 350, height: 200)
-                HStack {
-                    VStack {
-                        Rectangle()
-                            .foregroundColor(.pink)
-                            .frame(width: 150, height: 70)
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 150, height: 70)
-                    }
-                    VStack {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width: 150, height: 70)
-                        Rectangle()
-                            .foregroundColor(.yellow)
-                            .frame(width: 150, height: 70)
-                    }
-                }
-            }
+                .foregroundColor(.red)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.yellow)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.brown)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.green)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
         }
         .padding()
     }
